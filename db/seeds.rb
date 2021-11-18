b4ebbabd170ec07d1183520bdb2fae2cb45f9638
Eiskristallen <qhuangb47@gmail.com>
2021-11-18 14:33:53 -0600

    feat:add books data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,87 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Book.create(
+  title:"The Real Anthony Fauci: Bill Gates, Big Pharma, and the Global War on Democracy and Public Health",
+  description:"As director of the National Institute of Allergy and Infectious Diseases (NIAID), Dr. Anthony Fauci dispenses $6.1 billion in annual taxpayer-provided funding for scientific research, allowing him to dictate the subject, content, and outcome of scientific health research across the globe. Fauci uses the financial clout at his disposal to wield extraordinary influence over hospitals, universities, journals, and thousands of influential doctors and scientists—whose careers and institutions he has the power to ruin, advance, or reward.",
+  price:17.59,
+  quantity:21,
+  author:"Robert F. Kennedy Jr.",
+)
+Book.create(
+  title:"Humans",
+  description:"Just when we need it, Humans reminds us what it means to be human . . . one of the most influential art projects of the decade.",
+  price:21,
+  quantity:55,
+  author:"Brandon Stanton",
+)
+Book.create(
+  title:"The Judge's List: A Novel",
+  description:"In The Whistler, Lacy Stoltz investigated a corrupt judge who was taking millions in bribes from a crime syndicate. She put the criminals away, but only after being attacked and nearly killed. Three years later, and approaching 40, she is tired of her work for the Florida Board on Judicial Conduct and ready for a change.",
+  price:15,
+  quantity:23,
+  author:"John Grisham",
+)
+Book.create(
+  title:"The Stranger in the Lifeboat: A Novel",
+  description:"What would happen if we called on God for help and God actually appeared? In Mitch Albom’s profound new novel of hope and faith, a group of shipwrecked passengers pull a strange man from the sea. He claims to be “the Lord.” And he says he can only save them if they all believe in him.",
+  price:15.21,
+  quantity:33,
+  author:"Mitch Albom",
+)
+Book.create(
+  title:"Guinness World Records 2022",
+  description:"Fully revised and updated, and with a bright new design, Guinness World Records 2022 provides a fascinating snapshot of our world today. Our editors have chosen to curate the book with environmental issues at the forefront of their mind, so we open with a chapter exploring what’s happening to our ecosystem and what superlative lengths people are going to make a difference. We also want to encourage readers to put their own record-breaking to good, so look out for projects that might inspire you to make a difference – so don’t just Discover Your World, Change Your World!",
+  price:14.97,
+  quantity:245,
+  author:"Guinness World Records",
+)
+Book.create(
+  title:"The 1619 Project: A New Origin Story",
+  description:"A dramatic expansion of a groundbreaking work of journalism, The 1619 Project: A New Origin Story offers a profoundly revealing vision of the American past and present.",
+  price:24.97,
+  quantity:55,
+  author:"Nikole Hannah-Jones",
+)
+Book.create(
+  title:"Dune",
+  description:"Soon to be a major motion picture directed by Denis Villeneuve, starring Timothée Chalamet, Josh Brolin, Jason Momoa, Zendaya, Rebecca Ferguson, Oscar Isaac, Javier Bardem, Dave Bautista, Stellan Skarsgård, and Charlotte Rampling.",
+  price:55.21,
+  quantity:0,
+  author:"Frank Herbert",
+)
+Book.create(
+  title:"How to Catch a Turkey",
+  description:"A turkey is running loose in a school right before a Thanksgiving play. Can YOU help catch it so the show can go on? Follow along as students turn their school upside down trying to catch the turkey, ending with a twist that ensures no turkeys are harmed (or eaten!). This hilariously zany children's picture book combines STEAM concepts and traps with a silly story and fun illustrations, perfect for starting a new holiday family tradition or giving as a Thanksgiving gift for kids ages 4 and up!",
+  price:15.26,
+  quantity:124,
+  author:"Adam Wallace",
+)
+Book.create(
+  title:"Big Shot Diary of a Wimpy Kid Book",
+  description:"After a disastrous field day competition at school, Greg decides that when it comes to his athletic career, he’s officially retired. But after his mom urges him to give sports one more chance, he reluctantly agrees to sign up for basketball.",
+  price:9.42,
+  quantity:123,
+  author:"Jeff Kinney",
+)
+Book.create(
+  title:"Bad Cruz",
+  description:"I would say Dr. Cruz Costello is my archenemy.",
+  price:3.96,
+  quantity:22,
+  author:"L.J. Shen",
+)
+Book.create(
+  title:"Brown Bear, Brown Bear, What Do You See?",
+  description:"A big happy frog, a plump purple cat, a handsome blue horse, and a soft yellow duck--all parade across the pages of this delightful book. Children will immediately respond to Eric Carle's flat, boldly colored collages. Combined with Bill Martin's singsong text, they create unforgettable images of these endearing animals.",
+  price:23.11,
+  quantity:23,
+  author:"Bill Martin Jr",
+)
+Book.create(
+  title:"The Dark Hours",
+  description:"There’s chaos in Hollywood at the end of the New Year’s Eve countdown. Working her graveyard shift, LAPD detective Renée Ballard waits out the traditional rain of lead as hundreds of revelers shoot their guns into the air. Only minutes after midnight, Ballard is called to a scene where a hardworking auto shop owner has been fatally hit by a bullet in the middle of a crowded street party.",
+  price:17.65,
+  quantity:13,
+  author:"Michael Connelly",
+)

cfc47021dccb4e4e0c95d0573c71a27943644b91
Eiskristallen <qhuangb47@gmail.com>
2021-11-29 08:35:13 -0600

    feat: load current user before login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-  before_action :load_categories
+  before_action :load_categories,:current_customer
+
   private
+  def current_customer
+    if current_account
+      @customer = current_account
+    end
+  end
   def load_categories
    @c = Category.all.map{ |u| [ u.name, u.id ] }
   end
